1f0bac89275fe60bcbe321587a617bd5058f894c
Huu Vuong <vuongit.dlu@gmail.com>
2019-05-01 09:13:19 +0700

    update front-end

---
+++ DuAn_Nhom10/DuAn_Nhom10/Scripts/login.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".btn-mid").click(function () {
+        $(".col-right").fadeOut("slow");
+        $(".btn-mid").toggleClass("btn-mid-new");
+        $(".btn-mid-new").removeClass("btn-mid");
+        $(".btn-mid-new").text("►");
+    });
+});
+$(document).ready(function () {
+    $(".btn-mid-new").click(function () {
+        $(".col-right").fadeIn("slow");
+        $(".btn-mid-new").toggleClass("btn-mid");
+        $(".btn-mid").removeClass("btn-mid-new");
+        $(".btn-mid").text("◀");
+    });
+});
