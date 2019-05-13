25ac8c432bea9c90743d95d99503f7a3794b123e
caokhanh <cao.khanh.1997@gmail.com>
2019-05-13 17:44:25 +0700

    commit admin lan 2

--- DuAn_Nhom10/DuAn_Nhom10/Areas/Admin/AdminAreaRegistration.cs
+++ DuAn_Nhom10/DuAn_Nhom10/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace DuAn_Nhom10.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index",controller="Home", id = UrlParameter.Optional }
             );
         }
     }
