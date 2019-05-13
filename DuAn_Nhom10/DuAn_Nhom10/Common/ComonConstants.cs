07a472790459ef84ffe9d8403d396addeddaec29
caokhanh <cao.khanh.1997@gmail.com>
2019-05-13 16:58:40 +0700

    commit admin lan 1

---
+++ DuAn_Nhom10/DuAn_Nhom10/Common/ComonConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuAn_Nhom10.Common
+{
+    public class ComonConstants
+    {
+        public static string USER_SESSION = "USER_SESSION";
+    }
+}
---
+++ DuAn_Nhom10/DuAn_Nhom10/Common/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuAn_Nhom10.Common
+{
+    [Serializable]
+    public class UserLogin
+    {
+        private long userID;
+        private string userName;
+
+        public long UserID { get => userID; set => userID = value; }
+        public string UserName { get => userName; set => userName = value; }
+    }
+}
